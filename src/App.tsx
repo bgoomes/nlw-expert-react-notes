5bbee5747b34d0d5dd9f59dbf56fd04140bd9eba
Bruno Gomes <bgomes.89@gmail.com>
2024-02-06 18:05:00 -0300

    Upadate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import Logo from './assets/Logo-nlw-expert.svg'
+
+
 export function App() {
   return(
-    <h1>Hello Word</h1>
+    <div className=' mx-auto max-w-6xl my-12'>
+       <img src={Logo} alt="Logo nlw Expert" />
+
+       <form className=' w-full'>
+         <input type="text" placeholder='Busque suas notas...'  className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500'/>
+       </form>
+      
+    </div>
+   
   )
 }
 
